b67de43052bf34859a32aa0bfc26b4d54ceb6873
Brian Gray <638937+brianmgray@users.noreply.github.com>
2024-01-19 15:40:57 -0500

    Update bin/new-env.sh Add .p10k.zsh

--- bin/new-env.sh
+++ bin/new-env.sh
@@ -1,53 +1,72 @@
 #!/bin/zsh
 # Run to setup new environment
 
-function verify() {
-    name=$1
-    expected=$2
-    actual=$3
+autoload -U colors && colors
+
+function print_message() {
+    local message="$1"
+    local color_code="${2:-black}"
+    echo $fg[$color_code]$message$reset_color
+}
+
+function run_if_needed() {
+    local name=$1
+    local expected=$2
+    local actual=$3
+    local command=$4
     
     if [ "$2" = "$3" ]; then
-        echo "Verified correct version for $name [$actual]"
+        print_message "\t$name: Verified correct version [$actual]" green
     else
-        echo "Incorrect version for $name. Expected [$expected]. Using [$actual]"
-        exit 1
+        print_message "\t$name: Incorrect version. Expected [$expected]. Using [$actual]. Running command." red
+        eval "$command"
+        print_message "\t$name: Success."
     fi
 }
 
 function run() {
     # upgrade brew and dependencies
-    echo "updating brew..."
-    brew update -v 
-    brew upgrade 
-    brew upgrade --cask 
-    brew cleanup
+    print_message "updating brew..." yellow
+    # brew update -v
+    # brew upgrade && brew upgrade --cask && brew cleanup
 
     # git
-    echo "setting up git..."
-    brew install git
+    print_message "setting up git..." yellow
     expected=$(brew list --versions git | cut -c 5-)    # git 2.43.0
     actual=$(git --version | cut -c 13-)                # git version 2.43.0
-    verify git $expected $actual
+    run_if_needed git "$expected" "$actual" "brew install git"
 
     # zsh
-    echo "setting up oh-my-zsh"
-    expected="omz not found"
-    actual=$(which omz)
-    if [ expected != actual ]; then echo "omz is found"; else echo "omz not found"; exit 1; fi
-    omz update
+    print_message "setting up oh-my-zsh..." yellow
+    expected="master"
+    actual=$(omz version | cut -c -6)                   # master (d43f03b)
+    run_if_needed "oh-my-zsh" "$expected" "$actual" "omz update"
+
+    # zsh theme
+    theme_dir=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    if [[ ! -d "$directory_path" ]]; then
+      echo "oh-my-zsh theme not found, pulling..."
+      git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$theme_dir"
+    fi
 
     # nvm
-    # pulled automatically by oh-my-zsh
-    echo "setting up chezmoi..."
-    nvm install --lts                                   # install long-term stable version
-    nvm install-latest-npm
-    # verify later
+    print_message "setting up nvm..." yellow
+    # Check if NVM is installed
+    NVM_PATH="$HOME/.nvm/nvm.sh"
+    if [ ! -f "$NVM_PATH" ]; then
+      PROFILE=/dev/null zsh -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | zsh'
+    fi
+    latest_stable_version=$(nvm version-remote --lts)
+    current_version=$(nvm current)
+    run_if_needed nvm "$latest_stable_version" "$current_version" "nvm install --lts && nvm install-latest-npm"
 
     # chezmoi
-    echo "setting up chezmoi..."
-    brew install chezmoi
+    print_message "setting up chezmoi..." yellow
+    expected="/usr/local/bin/chezmoi"
+    actual=$(which chezmoi)
+    run_if_needed "chezmoi" "expected" "actual" "brew install chezmoi"
 
-    echo "Successfully setup new environment"
+    print_message "Successfully setup new environment" green
 }
 
 run

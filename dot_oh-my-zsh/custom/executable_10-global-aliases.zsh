d45979ee5a7fdb4ad8ccb4eefc0b92cd28891d12
Brian Gray <638937+brianmgray@users.noreply.github.com>
2024-02-05 15:04:47 -0500

    Update .oh-my-zsh/custom/10-global-aliases.zsh

--- dot_oh-my-zsh/custom/executable_10-global-aliases.zsh
+++ dot_oh-my-zsh/custom/executable_10-global-aliases.zsh
@@ -18,7 +18,7 @@ alias cp-from-envfiles='~/workspaces/environment-files/bin/cp-from-envfiles'
 
 # dirs
 alias dcc='~/workspaces/RainyMrGab/comedy-connector-mono'
-
+ 
 # dojo
 # alias mvn='mvn -Djavax.net.ssl.trustStore="~/certs/trust.jks"'
 # alias gradle='gradle -Djavax.net.ssl.trustStore="~/trust.jks"'

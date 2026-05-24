1611852d53ad65065003d6a124dcc1bbc6eded12
Brian Gray <638937+brianmgray@users.noreply.github.com>
2026-05-24 10:13:49 -0400

    misc updates

--- dot_oh-my-zsh/custom/executable_01-exports.zsh
+++ dot_oh-my-zsh/custom/executable_01-exports.zsh
@@ -1,6 +1,11 @@
 # Any exports needed 
 
+# nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# docker
+export DOCKER_BUILDKIT=1
+export COMPOSE_DOCKER_CLI_BUILD=1
 
